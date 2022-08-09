6062a30c826c33d6e81bb2fede0bb50e64c5708c
Christian Rafael <Crvc1998@gmail.com>
2022-08-09 17:01:00 -0500

    fix: foreign key definition for product

--- src/infrastructure/entities/product_entity.py
+++ src/infrastructure/entities/product_entity.py
@@ -2,6 +2,8 @@ from sqlalchemy import (
     MetaData, Table, Column, Integer, Text, Numeric, Boolean, ForeignKey
 )
 
+from src.infrastructure.entities import CategoryEntity
+
 metadata = MetaData()
 
 ProductEntity = Table(
@@ -13,5 +15,5 @@ ProductEntity = Table(
     Column('price', Numeric, nullable=False),
     Column('pvp', Numeric, nullable=False),
     Column('has_discount', Boolean, nullable=False),
-    Column('category_id', Integer, ForeignKey("categories.id"), nullable=False),
+    Column('category_id', Integer, ForeignKey(CategoryEntity.c.id), nullable=False),
 )
