e067a10a8e2edf582e92f9f8154ca1510d1a49f8
Christian Rafael <Crvc1998@gmail.com>
2022-08-10 12:02:04 -0500

    feat: add category injection

--- src/configuration.py
+++ src/configuration.py
@@ -3,6 +3,8 @@ import os
 import inject
 from flask import Flask
 
+from src.domain.category.category_service_impl import CategoryServiceImpl
+from src.domain.category.input.category_service import CategoryService
 from src.domain.category.output.category_repository import CategoryRepository
 from src.domain.product.output.product_repository import ProductRepository
 from src.infrastructure.adapters.input.repositories.category_repository_impl import CategoryRepositoryImpl
@@ -19,6 +21,7 @@ def configure_inject(application: Flask) -> None:
     def config(binder: inject.Binder) -> None:
         binder.bind(CategoryRepository, CategoryRepositoryImpl(application.config['DATABASE_URI']))
         binder.bind(ProductRepository, ProductRepositoryImpl(application.config['DATABASE_URI']))
+        binder.bind(CategoryService, CategoryServiceImpl(CategoryRepositoryImpl(application.config['DATABASE_URI'])))
 
     inject.configure(config)
 
